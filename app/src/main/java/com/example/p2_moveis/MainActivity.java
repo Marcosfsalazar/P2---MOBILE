44395afeec0504a5d1b001d3c2bb7a9455630a8c
Marcos Salazar <Marcos1010521@gmail.com>
2020-11-23 19:55:09 -0400

    main com campos para cadastrar

--- app/src/main/java/com/example/p2_moveis/MainActivity.java
+++ app/src/main/java/com/example/p2_moveis/MainActivity.java
@@ -4,12 +4,21 @@ package com.example.p2_moveis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtName;
+    EditText txtIdade;
+    Button buttonAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtName = (EditText)findViewById(R.id.txtName);
+        txtIdade = (EditText)findViewById(R.id.txtIdade);
     }
 }
